5f5f7d2384efd3d962c32cafb0da1c96e1a823af
harshank1122 <harshankpatil@gmail.com>
2025-06-04 01:03:03 +0530

    changebacport1

--- student-form-app/dist/student-form-app/server/angular-app-manifest.mjs
+++ student-form-app/dist/student-form-app/server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 498, hash: '5b315a529bf50bb388eb4efd1fcbc6b097842eed77b795a48ffdf8c99adaa3a5', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1011, hash: '0fad2a03d254265b700d835e21f2837be3c3001d3350758b06172b3eecd7172b', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 2407, hash: 'a2e2ba749751e8abc4a6212d015f061de60822662aafd277f85090be13f02d56', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 498, hash: 'f45eb35f8e9401b41e3585a2fab2b991d6d784ee42a356b220a95016527b9bd1', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1011, hash: '2d7396b9f8860a81ac71b74a62b3b63be0f97ef0bcbbadf0cdce10a81db83c8d', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 2407, hash: '89960b48fc6130c1c05523eec9a1f00075d8a169ca9b92152a858357dc99fa1d', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-5INURTSO.css': {size: 0, hash: 'menYUTfbRu8', text: () => import('./assets-chunks/styles-5INURTSO_css.mjs').then(m => m.default)}
   },
 };
