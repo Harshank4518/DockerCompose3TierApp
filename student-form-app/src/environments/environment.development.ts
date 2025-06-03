a4a49a448ff43caedce52798771838f8abecb0f6
harshank1122 <harshankpatil@gmail.com>
2025-06-03 13:02:06 +0530

    ipupdate

--- student-form-app/src/environments/environment.development.ts
+++ student-form-app/src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000'
+  apiUrl: 'http://184.73.146.206:3000'
 };
--- student-form-app/src/environments/environment.ts
+++ student-form-app/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000'
+  apiUrl: 'http://184.73.146.206:3000'
 };
